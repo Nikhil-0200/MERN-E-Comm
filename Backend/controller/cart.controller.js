bc921dcdf7f3a19eedb83b9b2b838a553e5cc9b7
Nikhil-0200 <nikhiltiwari0200@gmail.com>
2024-12-29 02:55:50 +0530

    Cart - Add, Update, FetchByUser, Delete features added in backend

--- Backend/controller/cart.controller.js
+++ Backend/controller/cart.controller.js
@@ -28,13 +28,25 @@ exports.deleteItems = async(req, res)=>{
     
     const {id} = req.params
     try {
-        const cartItems = await cartModel.findOneAndDelete({product: id});
+        const cartItems = await cartModel.findByIdAndDelete(id);
         if (!cartItems) {
             return res.status(404).json({ msg: "Item not found" });
         }
         res.status(200).json({ id }); // Return the ID after deletion
     } catch (error) {
         res.status(400).json({msg: `Error occurred while deleting cart items ${error}`})
+    }
+}
+
+exports.updateItems = async(req, res) =>{
+    const {id} = req.params;
+
+    try {
+        const updatedData = await cartModel.findByIdAndUpdate(id, req.body, {new: true});
+        await updatedData.populate("product");
+        res.status(200).json(updatedData);    
+    } catch (error) {
+        res.status(400).json({msg: `Error occurred while updating cart item ${error}`})
     }
 }
 
--- Backend/routes/cart.router.js
+++ Backend/routes/cart.router.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const { addToCart, fetchCartByUser, deleteItems } = require("../controller/cart.controller");
+const {
+  addToCart,
+  fetchCartByUser,
+  deleteItems,
+  updateItems,
+} = require("../controller/cart.controller");
 
 const cartRouter = express.Router();
 
-cartRouter.post("/", addToCart).get("/:id", fetchCartByUser).delete("/:id", deleteItems);
+cartRouter
+  .post("/", addToCart)
+  .get("/:id", fetchCartByUser)
+  .delete("/:id", deleteItems)
+  .patch("/:id", updateItems);
 
 module.exports = cartRouter;
