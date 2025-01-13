cffb8afa9f47a3d9dcee92e65ca4314f5e6f0823
Nikhil-0200 <nikhiltiwari0707@gmail.com>
2025-01-14 02:19:41 +0530

    Test

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -1,26 +1,29 @@
 const blacklistedToken = require("../blacklist");
 const jwt = require("jsonwebtoken");
 
+const authMiddleware = (req, res, next) => {
+  const accessToken = req.cookies.accessToken;
+  // const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3N2QzMWE3ZmNmZDNmYjc4ZDlmNTIyNyIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTczNjcyMTE1NCwiZXhwIjoxNzM2ODA3NTU0fQ.Aesn-p5m83_izdPKrNqt05quq_s2eIZb0mCQb-pS1Q0";
 
-const authMiddleware = (req, res, next) =>{
-    const accessToken = req.cookies.accessToken;
-    // const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3N2QzMWE3ZmNmZDNmYjc4ZDlmNTIyNyIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTczNjcyMTE1NCwiZXhwIjoxNzM2ODA3NTU0fQ.Aesn-p5m83_izdPKrNqt05quq_s2eIZb0mCQb-pS1Q0";
+  if (blacklistedToken.includes(accessToken)) {
+    res.status(401).json({ msg: "Unauthorized" });
+  }
 
-    if(blacklistedToken.includes(accessToken)){
-        res.status(401).json({msg: "Unauthorized"});
+  jwt.verify(accessToken, process.env.JWT_SCERETKEY_1, (err, decoded) => {
+    if (err) {
+      const msg =
+        err.name === "TokenExpiredError"
+          ? "Session expired. Please login again."
+          : "Unauthorized - Invalid token.";
+      return res.status(401).json({ msg });
     }
 
-    jwt.verify(accessToken, process.env.JWT_SCERETKEY_1, (err, decoded)=>{
-        if(err){
-            res.status(401).json({msg: "Unauthorized / Login First"});
-        }
-
-        if(decoded){
-            req.body.userId = decoded.id;
-            req.body.role = decoded.role;
-            next();
-        }
-    });
-}
+    if (decoded) {
+      req.body.userId = decoded.id;
+      req.body.role = decoded.role;
+      next();
+    }
+  });
+};
 
 module.exports = authMiddleware;
